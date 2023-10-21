5fdecb66c00db641165bb569d372686c11d94b8d
Kacper Mazur <109112557+mazur-kacper@users.noreply.github.com>
2023-10-21 19:16:02 +0200

    Store display value in a String

--- script.js
+++ script.js
@@ -17,7 +17,20 @@ function divide(a, b) {
 let firstNumber;
 let secondNumber;
 let operator;
+let displayValue = "";
 
 function operate(firstNumber, operator, secondNumber) {
   return operator(firstNumber, secondNumber);
 }
+
+const displayScreen = document.querySelector("#calculator-screen");
+
+const numberButtons = document.querySelectorAll(".number-button");
+
+numberButtons.forEach(function (button) {
+  button.addEventListener("click", (event) => {
+    displayScreen.textContent += button.textContent;
+    displayValue += button.textContent;
+    console.log(displayValue);
+  });
+});

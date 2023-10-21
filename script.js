4b58ba6909e7f2766f6bf527ddc477d2cfbc9950
Kacper Mazur <109112557+mazur-kacper@users.noreply.github.com>
2023-10-21 22:16:11 +0200

    Comment code

--- script.js
+++ script.js
@@ -20,17 +20,53 @@ let operator;
 let displayValue = "";
 
 function operate(firstNumber, operator, secondNumber) {
-  return operator(firstNumber, secondNumber);
+  switch (operator) {
+    case "add":
+      return add(firstNumber, secondNumber);
+    case "subtract":
+      return subtract(firstNumber, secondNumber);
+    case "multiply":
+      return multiply(firstNumber, secondNumber);
+    case "divide":
+      return divide(firstNumber, secondNumber);
+  }
 }
 
 const displayScreen = document.querySelector("#calculator-screen");
 
 const numberButtons = document.querySelectorAll(".number-button");
 
+const operatorButtons = document.querySelectorAll(".operator-button");
+
+const equalsButton = document.querySelector("#equals-button");
+
 numberButtons.forEach(function (button) {
   button.addEventListener("click", (event) => {
     displayScreen.textContent += button.textContent;
     displayValue += button.textContent;
-    console.log(displayValue);
   });
 });
+
+// operatorButtons.forEach(function (button) {
+//   button.addEventListener("click", (event) => {
+//     firstNumber = Number(displayValue);
+//     operator = String(button.id);
+//     button.style.cssText = "color: red;";
+//     displayValue = "";
+//   });
+// });
+
+// if (operator !== null) {
+//   displayScreen.textContent = " ";
+//   numberButtons.forEach(function (button) {
+//     button.addEventListener("click", (event) => {
+//       displayScreen.textContent += button.textContent;
+//       displayValue += button.textContent;
+//     });
+//   });
+// }
+
+// equalsButton.addEventListener("click", (event) => {
+//   displayScreen.textContent = "";
+//   console.log(operate(firstNumber, operator, secondNumber));
+// });

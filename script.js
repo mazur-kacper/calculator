c982e69b4522d76226df79c6423dd6a98cd76769
Kacper Mazur <109112557+mazur-kacper@users.noreply.github.com>
2023-10-31 03:16:24 +0100

    Change display style

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  if (b === 0) {
+    return (displayScreen.textContent = "ERROR");
+  } else {
+    return a / b;
+  }
 }
 
 let firstNumber;
 let secondNumber;
 let operator;
 let displayValue = "";
+let storedNumbers = [];
+let hasBeenPerformed;
 
 function operate(firstNumber, operator, secondNumber) {
   switch (operator) {
@@ -33,55 +39,108 @@ function operate(firstNumber, operator, secondNumber) {
 }
 
 const displayScreen = document.querySelector("#calculator-screen");
-
 const numberButtons = document.querySelectorAll(".number-button");
-
 const operatorButtons = document.querySelectorAll(".operator-button");
-
 const equalsButton = document.querySelector("#equals-button");
+const clearButton = document.querySelector("#ac-button");
+const backSpaceButton = document.querySelector("#backspace-button");
 
-function setupButtons() {
+function digitButtons() {
   numberButtons.forEach(function (button) {
     button.addEventListener("click", (event) => {
-      displayScreen.textContent += button.textContent;
+      if (hasBeenPerformed) {
+        displayScreen.textContent = "";
+      }
+      if (displayScreen.textContent === "0") {
+        displayScreen.textContent = button.textContent;
+      } else {
+        displayScreen.textContent += button.textContent;
+      }
       displayValue += button.textContent;
-      console.log(displayValue);
     });
+    hasBeenPerformed = false;
   });
 }
 
 function getInput() {
   return Number(displayValue);
 }
+
+function backspace() {
+  backSpaceButton.addEventListener("click", (event) => {
+    if (displayScreen.textContent.length === 1) {
+      if (displayScreen.textContent === "0") {
+      } else {
+        displayScreen.textContent = "0";
+      }
+    } else {
+      displayScreen.textContent = displayScreen.textContent.slice(0, -1);
+    }
+  });
+}
+
 function utilizeOperator() {
   operatorButtons.forEach(function (button) {
     button.addEventListener("click", (event) => {
-      firstNumber = getInput();
-      console.log(firstNumber);
-      operator = String(button.id);
-      displayScreen.textContent = "";
+      hasBeenPerformed = true;
+      if (storedNumbers.length === 0) {
+        number = getInput();
+        operator = String(button.id);
+        storedNumbers.push(number);
+        number = "";
+        console.log(storedNumbers);
+      } else if (storedNumbers.length === 1) {
+        number = getInput();
+        storedNumbers.push(Number(number));
+        result = String(operate(storedNumbers[0], operator, storedNumbers[1]));
+        displayScreen.textContent = `${result} `;
+        storedNumbers = [];
+        storedNumbers.push(Number(result));
+        operator = String(button.id);
+        number = "";
+        console.log(storedNumbers);
+      }
       displayValue = "";
+      number = "";
     });
+  });
+}
+
+function clear() {
+  clearButton.addEventListener("click", (event) => {
+    storedNumbers = [];
+    displayScreen.textContent = "0";
+    operator = "";
+    displayValue = "";
   });
 }
 
 function calculate() {
   equalsButton.addEventListener("click", (event) => {
-    displayScreen.textContent = "";
-    secondNumber = getInput();
-    result = String(operate(firstNumber, operator, secondNumber));
-    displayScreen.textContent = "";
+    number = getInput();
+    storedNumbers.push(number);
+    result = String(operate(storedNumbers[0], operator, storedNumbers[1]));
+    displayScreen.textContent = `${result} `;
+    storedNumbers = [];
+    storedNumbers.push(Number(result));
+    number = "";
+    displayValue = "";
+    console.log(storedNumbers);
 
-    displayScreen.textContent = result;
-    displayValue = result;
-    console.log(displayValue);
+    //   .toFixed(7)
+    //   .replace(/\.?0+$/, "");
+    // displayValue = Number(result)
+    //   .toFixed(7)
+    //   .replace(/\.?0+$/, "");
   });
 }
 
 function calculator() {
-  setupButtons();
+  clear();
+  digitButtons();
   utilizeOperator();
   calculate();
+  backspace();
 }
 
 calculator();
